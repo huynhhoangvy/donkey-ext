5eaea0837edaf49df31c4cf026dbe77f706ab7f7
V <huynh.h.vy@gmail.com>
2021-04-23 20:03:38 +0700

    demo extension

---
+++ assets/js/options.js
@@ -0,0 +1,45 @@
+const page = document.getElementById('buttonDiv');
+const selectedClassName = 'current';
+const presetButtonColors = ['#3aa757', '#e8453c', '#f9bb2d', '#4688f1'];
+
+// Reacts to a button click by marking the selected button and saving the selection
+const handleButtonClick = e => {
+	// Remove styles from the previously selected color
+	const current = e.target.parentElement.querySelector(
+		`.${selectedClassName}`,
+	);
+	if ( current && current !== e.target ) {
+		current.classList.remove(selectedClassName);
+	}
+
+	// Mark the button as selected
+	const color = e.target.dataset.color;
+	e.target.classList.add(selectedClassName);
+	chrome.storage.sync.set({ color });
+};
+
+// Add a button to the page for each supplied color
+const constructOptions = buttonColors => {
+	chrome.storage.sync.get('color', data => {
+		const currentColor = data.color;
+		// For each color provided
+		for ( let buttonColor of buttonColors ) {
+			// ...create a button with that color...
+			const button = document.createElement('button');
+			button.dataset.color = buttonColor;
+			button.style.backgroundColor = buttonColor;
+
+			// ...mark the currently selected color...
+			if ( buttonColor === currentColor ) {
+				button.classList.add(selectedClassName);
+			}
+
+			// ...and register a listener for when that button is clicked
+			button.addEventListener('click', handleButtonClick);
+			page.appendChild(button);
+		}
+	});
+};
+
+// Initialize the page by constructing the color options
+constructOptions(presetButtonColors);
